bc64bdcbc457a54622afc705889f8f2cd3bdc7ec
s68122250048 <s68122250008@ssru.ac.th>
2026-02-26 10:09:08 +0700

    Add Files

---
+++ Example.java
@@ -0,0 +1,29 @@
+public class Example {
+    private int count = 0;
+
+    public static void main(String[] args) {
+        try {
+            Example ex = new Example();
+            ex.f();
+            System.out.println(ex.count);
+        } catch (Exception e) {
+            System.out.println("Boom!");
+        }
+    }
+
+    public void f() throws Exception {
+        add(2);
+        add(3);
+    }
+
+    public void add(int val) throws Exception {
+        if (val == 2) {
+            throw new Exception();
+        }
+        count += val;
+    }
+
+    public void mult(int val) {
+        count *= val;
+    }
+}
